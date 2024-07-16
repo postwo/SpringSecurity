eefdbf325f0712416a31bb04b5550905e00ec558
postwo <codo7717@naver.com>
2024-07-17 04:06:58 +0900

    회원가입 연습

--- src/main/java/com/example/springsecurity/controller/IndexController.java
+++ src/main/java/com/example/springsecurity/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.example.springsecurity.controller;
 
+import com.example.springsecurity.model.User;
+import com.example.springsecurity.reposiotry.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @GetMapping({"","/"})
     public String index(){
@@ -28,18 +37,25 @@ public class IndexController {
         return "manager";
     }
 
-    @GetMapping("/login")
-    public @ResponseBody String login(){
-        return "login";
+    //로그인 페이지
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    public @ResponseBody String join(){
+    //회원가입 페이지
+    @GetMapping("/joinForm")
+    public  String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        user.setRole("USER");
+        String encPassword  = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encPassword);
+        userRepository.save(user);
         return "join";
     }
 
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨";
-    }
 }
---
+++ src/main/java/com/example/springsecurity/model/User.java
@@ -0,0 +1,25 @@
+package com.example.springsecurity.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@Entity
+public class User {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role; //USER, ADMIN
+    @CreationTimestamp
+    private Timestamp createDate;
+}
